11d652a8db5a7fb39b40d70466e65473472d6358
Saif Abrar <62766337+saifabrar17@users.noreply.github.com>
2022-03-15 19:33:06 +0600

    reacting with button

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-document.getElementById('case-plus').addEventListener('click', function(){
-    console.log("case plus button clicked");
+//working with case plus btn
+document.getElementById('case-plus').addEventListener('click',
+    function () {
+        const caseInput = document.getElementById('case-number');
+        const caseNumber = caseInput.value;
+        caseInput.value = parseInt(caseNumber) + 1; 
+    });
+
+    //working with case MINUS btn
+document.getElementById('case-minus').addEventListener('click', function(){
+    const caseInput = document.getElementById('case-number');
+    const caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) - 1;
 })
