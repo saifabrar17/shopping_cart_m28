da39c3cbd3801ca6daf376e9cb4ed197a0851dfa
Saif Abrar <62766337+saifabrar17@users.noreply.github.com>
2022-03-15 21:20:52 +0600

    nesting function, 1 function 3param, 2 case, phone and case

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-function updateCaseNumber (isIncreasing){
-    const caseInput = document.getElementById('case-number');
+function updateCaseNumber (product, price, isIncreasing){
+    const caseInput = document.getElementById(product + '-number');
     let caseNumber = caseInput.value;
     if(isIncreasing){
         caseNumber  = parseInt(caseNumber) + 1; 
@@ -8,17 +8,30 @@ function updateCaseNumber (isIncreasing){
         caseNumber = parseInt(caseNumber) - 1;
     }
     caseInput.value = caseNumber;
-    const caseTotal = document.getElementById('case-total');
-    caseTotal.innerText = caseNumber * 59;
+    const caseTotal = document.getElementById(product + '-total');
+    caseTotal.innerText = caseNumber * price;
 }
+
+//working with phone plus btn
+document.getElementById('phone-plus').addEventListener('click',
+    function () {
+        updateCaseNumber('phone', 1219, true);
+    });
+
+//working with phone MINUS btn
+document.getElementById('phone-minus').addEventListener('click', function(){
+    updateCaseNumber('phone', 1219, false);
+})
+
+
 
 //working with case plus btn
 document.getElementById('case-plus').addEventListener('click',
     function () {
-        updateCaseNumber(true);
+        updateCaseNumber('case', 59, true);
     });
 
 //working with case MINUS btn
 document.getElementById('case-minus').addEventListener('click', function(){
-    updateCaseNumber(false);
+    updateCaseNumber('case', 59, false);
 })
